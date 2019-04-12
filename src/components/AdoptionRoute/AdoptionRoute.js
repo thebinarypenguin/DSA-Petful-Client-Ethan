66b7196f376021b7946b7468f35be67e6d5e01c5
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-12 15:12:46 -0400

    Styling routes

--- src/components/AdoptionRoute/AdoptionRoute.js
+++ src/components/AdoptionRoute/AdoptionRoute.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import Pet from '../Pet/Pet';
 
-class App extends React.Component {
+import './AdoptionRoute.css';
+
+class AdoptionRoute extends React.Component {
+
+  handleFormSubmit(ev) {
+
+    ev.preventDefault();
+
+    // get adoptCat
+    // get adoptDog
+
+    // fetch
+      // good
+        // replace adopt button with Congratulations
+      // err
+        // put error message above the adopt button
+        console.log('submit')
+  }
 
   render() {
     return (
       <div id="AdoptionRoute">
-        <p>this is the adoption route</p>
+        <form onSubmit={this.handleFormSubmit}>
+          <div className="PetContainer">
+            <Pet type="cat"/>
+            <Pet type="dog"/>
+          </div>
+          <div className="ButtonContainer">
+            <button className="Button">Adopt</button>
+          </div>
+        </form>
+
       </div>
     );
   }
 }
 
-export default App;
+export default AdoptionRoute;
--- src/components/HomeRoute/HomeRoute.js
+++ src/components/HomeRoute/HomeRoute.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 
 import './HomeRoute.css';
 
-class App extends React.Component {
+class HomeRoute extends React.Component {
 
   render() {
     return (
       <div id="HomeRoute">
         <p>this is the home route</p>
 
-        <div class='ButtonContainer'>
-          <Link class="Button" to='/adopt'>Start</Link>
+        <div className='ButtonContainer'>
+          <Link className="Button" to='/adopt'>Start</Link>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default HomeRoute;
