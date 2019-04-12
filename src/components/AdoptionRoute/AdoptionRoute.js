a82d1a62463b983d142a0f63f6377dd283ce7a6e
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-12 14:43:17 -0400

    Create AdoptionRoute component

---
+++ src/components/AdoptionRoute/AdoptionRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div id="AdoptionRoute">
+        <p>this is the adoption route</p>
+      </div>
+    );
+  }
+}
+
+export default App;
