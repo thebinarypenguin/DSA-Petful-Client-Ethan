53c1a1dead8322838720dcc5a2a56d198369c4fc
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-12 18:15:13 -0400

    Styleing

--- src/components/Pet/Pet.js
+++ src/components/Pet/Pet.js
@@ -2,7 +2,29 @@ import React from 'react';
 import './Pet.css';
 import ApiService from '../services/ApiService';
 
+import './Pet.css';
+
 class Pet extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pet: null,
+    };
+  }
+
+  componentDidMount() {
+
+    if (this.props.type === 'cat') {
+      ApiService.getCat().then((cat) => { this.setState({ pet: cat }); })
+    }
+
+    if (this.props.type === 'dog') {
+
+      ApiService.getDog().then((dog) => { this.setState({ pet: dog }); })
+    }
+  }
 
   handleAdoptCat = () => {
     ApiService.adoptCat();
@@ -14,31 +36,58 @@ class Pet extends React.Component {
 
   render() {
 
+    let details;
+
+    if (this.state.pet) {
+      details = (
+        <>
+        <img src={this.state.pet.imageURL} alt={this.state.pet.imageDescription} />
+
+        <table>
+          <tbody>
+          <tr>
+            <th>Name</th>
+            <td>{this.state.pet.name}</td>
+          </tr>
+          <tr>
+            <th>Sex</th>
+            <td>{this.state.pet.sex}</td>
+          </tr>
+          <tr>
+            <th>Age</th>
+            <td>{this.state.pet.age}</td>
+          </tr>
+          <tr>
+            <th>Breed</th>
+            <td>{this.state.pet.breed}</td>
+          </tr>
+          <tr>
+            <th>Story</th>
+            <td>{this.state.pet.story}</td>
+          </tr>
+          </tbody>
+        </table>
+        </>
+      );
+    }
+
     let button;
 
     if (this.props.type === 'cat') {
       button = (
-          <input name="adoptCat" type="button" value='Adopt this cat' onClick={this.handleAdoptCat}></input>
+          <input name="adoptCat" type="button" value='Adopt this cat' className="Button" onClick={this.handleAdoptCat}></input>
       );
     }
 
     if (this.props.type === 'dog') {
       button = (
-          <input name="adoptDog" type="button" value='Adopt this dog' onClick={this.handleAdoptDog}></input>
+          <input name="adoptDog" type="button" value='Adopt this dog' className="Button" onClick={this.handleAdoptDog}></input>
       );
     }
 
     return (
       <div className="Pet">
-
-        <p>imageURL</p>
-        <ul>
-          <li>name</li>
-          <li>sex</li>
-          <li>age</li>
-          <li>breed</li>
-          <li>story</li>
-        </ul>
+        { details }
         { button }
       </div>
     );
